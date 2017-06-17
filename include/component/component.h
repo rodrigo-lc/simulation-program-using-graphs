3dcc285b2bebedbc6e816e47718a399ab7e543e7
rodrigo-lc <rodrigoluizdacosta@outlook.com.br>
2017-06-17 00:18:37 -0300

    DFS and BFS implementation
    Minor changes

---
+++ include/component/component.h
@@ -0,0 +1,22 @@
+/*
+ ==============================================================================
+ Name:              component.h
+ Contributors:      Caio Felipe Campoy, Rodrigo Luiz da Costa
+ Created on:        16/06/2017
+ Version:           1.0
+ Last modification: 16/06/2017
+ Copyright:         MIT License
+ Description:       Electric components structures for using in applications using graphs
+                    - header file
+ ==============================================================================
+ */
+
+#ifndef COMPONENT_H_INCLUDED
+#define COMPONENT_H_INCLUDED
+
+typedef struct resistors resistor_t;
+typedef struct voltageSources voltageSource_t;
+
+
+
+#endif // COMPONENT_H_INCLUDED
